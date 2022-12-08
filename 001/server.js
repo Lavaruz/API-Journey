cd6b1f43d0e40bb5c739f1865aaedd1476d81730
Assami Muzaki <muzakiassami@gmail.com>
2022-12-08 22:01:06 +0700

    auto format on save

--- 001/server.js
+++ 001/server.js
@@ -1,17 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/api', (req,res)=>{
-    res.json({status: 'OK'})
-})
-app.post('/api', (req,res)=>{
-    try{
-        res.status(201).json(req.body)
-    }catch(err){
-        res.json({message: err.message})
-    }
-})
+app.get('/', (req, res) => {
+    res.json({ status: 'OK' });
+});
 
-app.listen(3000, ()=> console.log('server run at port 3000'))
+app.listen(3000, () => console.log('server run at port 3000'));
